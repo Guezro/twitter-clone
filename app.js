786e24eefde5013358c3a15e55151b9c5ac270ac
Guezro <marcos.rodriguez.romero@hotmail.com>
2021-03-16 14:38:29 +0100

    last api version

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
+
+require('dotenv').config();
+
+
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,12 +35,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
 
 // error handler
 app.use(function(err, req, res, next) {
